b884ffc69c6afb82147deacf5ebf6fe6590ef65f
luis <luis@VAIO>
2017-01-09 22:25:12 -0500

    Se agregó Controles Base

---
+++ TEC.ControlesBase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEC.ControlesBase
+{
+    public class Class1
+    {
+    }
+}
