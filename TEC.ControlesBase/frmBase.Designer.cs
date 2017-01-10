4fe3e930bf9eff3ee637b18e393d6e56be9bf787
install-world <install-world@JOSELUIS>
2017-01-09 23:02:41 -0500

    Nuevo

--- TEC.ControlesBase/frmBase.Designer.cs
+++ TEC.ControlesBase/frmBase.Designer.cs
@@ -34,16 +34,16 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(12, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Dog";
+            this.button1.Text = "Nuevo";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(92, 21);
+            this.button2.Location = new System.Drawing.Point(160, 21);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
